e9060a1d5159a9dfd39834af09994c14669d3936
ayachuu <hahihuheho-ayachu-buu.0528@mineo.jp>
2021-07-08 12:39:33 +0900

    commentsコントローラー作成とルーテイング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "prototypes#index"
-  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+    resources :comments, only: :create
+  end
 end
