40faf1f4341b624e9b5d9a0f67bc5f671d55ec55
ayachuu <hahihuheho-ayachu-buu.0528@mineo.jp>
2021-07-08 17:09:41 +0900

    commentsコントローラー　createアクション

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "prototypes#index"
   resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
-    resources :comments, only: :create
+    resources :comments, only: [:create]
   end
 end
